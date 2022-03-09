5ea4d3d8504d48a2854154a2857128f411eef88e
Sarah <sarahlchavez@gmail.com>
2022-03-09 12:45:57 -0800

    added navbar link to home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Pages from './components/Pages';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Pages />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/'>Home</Link>
+                </div>
+            </nav>
+        </>
+    )
+}
