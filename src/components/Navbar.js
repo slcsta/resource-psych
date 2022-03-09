b721312fd7494add8c680652664b199d499970de
Sarah <sarahlchavez@gmail.com>
2022-03-09 13:08:33 -0800

    added navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo'></Link>
-                    <div className='menu-icon'></div>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    <Link to='/' className='navbar-logo'>Resource Psychiatry</Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+
+                    </ul>
                     <Link to='/'>Home</Link>
                 </div>
             </nav>
