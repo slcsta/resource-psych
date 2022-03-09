3f124d67f80b885f727467e2197d55ebbeaeb787
Sarah <sarahlchavez@gmail.com>
2022-03-09 12:54:26 -0800

    added navbar links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import Pages from './components/Pages';
-
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Pages />
     </div>
   );
 }
