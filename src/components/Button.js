57ff3a09884dd26bd9d533ee277f0fff2beab88c
Sarah <sarahlchavez@gmail.com>
2022-03-10 04:11:05 -0800

    added nav links and created button files

---
+++ src/components/Button.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import './Butoon.css'
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,30 @@ function Navbar() {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
     
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo'>Resource Psychiatry</Link>
+                    <Link to='/' className='navbar-logo'>
+                        Resource Psychiatry <i className='fab fa-typo3' />
+                    </Link>
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-
+                        <li className='nav-item'> 
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link></li>
+                        <li className='nav-item'> 
+                            <Link to='/meet-dr-soden' className='nav-links' onClick={closeMobileMenu}>Meet Dr. Soden</Link></li>
+                        <li className='nav-item'> 
+                            <Link to='/treatment' className='nav-links' onClick={closeMobileMenu}>Treatment</Link></li>
+                        <li className='nav-item'> 
+                            <Link to='/scheduling' className='nav-links' onClick={closeMobileMenu}>Scheduling</Link></li>
+                        <li className='nav-item'> 
+                            <Link to='/faq' className='nav-links' onClick={closeMobileMenu}>FAQs</Link></li>
                     </ul>
-                    <Link to='/'>Home</Link>
                 </div>
             </nav>
         </>
