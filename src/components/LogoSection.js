157bd42fbfc947fd11d3de7f792071e617c77960
Sarah <sarahlchavez@gmail.com>
2022-03-10 13:51:04 -0800

    added video and logo section files

---
+++ src/components/LogoSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LogoSection() {
+    return (
+        <div className='logo-container'>
+            <video src="/videos/video-1.mp4" autoPlay loop muted />
+            <h1>Resource Psychiatry</h1>
+        </div>
+
+    )
+}
