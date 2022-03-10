f51da64901b035cf2b066ce6df888e93e4bf733d
Sarah <sarahlchavez@gmail.com>
2022-03-10 04:59:32 -0800

    adding set state and logic for button size and style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,18 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
     
     return (
         <>
@@ -17,6 +26,7 @@ function Navbar() {
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
+                    {button && <Button buttonStyle='btn--outline'>New Patients</Button>}
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className='nav-item'> 
                             <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link></li>
