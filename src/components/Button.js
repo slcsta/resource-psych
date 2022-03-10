ff4b67651d0a72e9d8d735ae2bab9a574beff1e0
Sarah <sarahlchavez@gmail.com>
2022-03-10 04:34:56 -0800

    added button and style and size for mobile display

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import './Butoon.css'
+import './Button.css'
 import {link} from 'react-router-dom'
+
+const STYLES = ['btn--primary', 'btn--outline'];
+
+const SIZES = [ 'btn--medium', 'btn--large'];
 
 export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
