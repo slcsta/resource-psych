5602f206fdd47579eb38a88c07dc64c0007f49dc
Sarah <sarahlchavez@gmail.com>
2022-03-10 04:17:22 -0800

    added button and logic for mobile display

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Butoon.css'
+import {link} from 'react-router-dom'
+
+export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+
+    const checkButtonsize = SIZES.includes(buttonSize) ? buttonSize
+}
