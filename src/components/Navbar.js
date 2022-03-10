3cbb1137e065aaca4a86aeb1d1bd2b14cd0b3936
Sarah <sarahlchavez@gmail.com>
2022-03-10 10:15:21 -0800

    updated css for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button.js';
@@ -18,20 +18,22 @@ function Navbar() {
         }
     };
 
+    useEffect(() => {
+        showButton();
+    }, []);
+
     window.addEventListener('resize', showButton);
     
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo'>
-                        Resource Psychiatry <i className='fab fa-typo3' />
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        Resource Psychiatry <i class='fab fa-typo3' />
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
-                    {button && <Button buttonStyle='btn--outline'>New Patients</Button>}
-                    {button && <Button buttonStyle='btn--outline'>Current Patients</Button>}
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className='nav-item'> 
                             <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link></li>
@@ -43,11 +45,15 @@ function Navbar() {
                             <Link to='/scheduling' className='nav-links' onClick={closeMobileMenu}>Scheduling</Link></li>
                         <li className='nav-item'> 
                             <Link to='/faq' className='nav-links' onClick={closeMobileMenu}>FAQs</Link></li>
+                        <li><Link to='/new-patients' className='nav-links-mobile' onclick={closeMobileMenu}>New Patients</Link></li>
+                        <li><Link to='/current-patients' className='nav-links-mobile' onclick={closeMobileMenu}>Current Patients</Link></li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>New Patients</Button>}
+                    {button && <Button buttonStyle='btn--outline'>Current Patients</Button>}
                 </div>
             </nav>
         </>
     )
 }
 
-export default Navbar
+export default Navbar;
