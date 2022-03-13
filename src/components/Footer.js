fc1a30cc12a5cc850af0446f3d98dcc461d06304
Sarah <sarahlchavez@gmail.com>
2022-03-13 07:09:07 -0700

    updated footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
     return (
         <div className='footer-container'>
-            
+            <div className='footer-links'>
+                <div className="footer-link-wrapper">
+                   <div className="footer-link-items">
 
+                       </div> 
+                </div>
+            </div>
         </div>
     )
 }
