3b5d59ebe612c5c1e96b921ad1435e94d2f17b48
Sarah <sarahlchavez@gmail.com>
2022-03-13 06:54:21 -0700

    added footer component to home and added email form to scheduling component

--- src/components/SchedulingSection.js
+++ src/components/SchedulingSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './SchedulingSection.css'
 import '../App.css'
+import { Button } from './Button.js';
 
 function SchedulingSection() {
     return (
@@ -8,7 +9,14 @@ function SchedulingSection() {
             <div className='scheduling-container'>
                 <h1>Scheduling</h1>
                 <h2>Add Button Links to Luminello</h2>
-                <h2>Add direct email form</h2>
+                <h2>Email form</h2>
+                <div className="input-areas">
+                    <form>
+                        <input type="email" name="email" placeholder="Your Email" className="form-input"/>
+                        <Button buttonStyle='btn--outlin'>Email Us</Button>
+                    </form>
+                </div>
+
             </div>
         </>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../App.css';
 import LogoSection from '../components/LogoSection';
 import Cards from '../components/Cards';
-
+import Footer from '../components/Footer';
 
 function Home() {
     return (
         <>
             <LogoSection />
             <Cards />
+            <Footer />
         </>
     );
 }
