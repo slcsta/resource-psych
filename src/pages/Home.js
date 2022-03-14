acfbd4b40987c46122d6bd00ba65090445ad1d5a
Sarah <sarahlchavez@gmail.com>
2022-03-14 10:41:01 -0700

    added welcome component and footer with css styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../App.css';
 import LogoSection from '../components/LogoSection';
 import Cards from '../components/Cards';
+import Welcome from '../components/Welcome';
 import Footer from '../components/Footer';
 
 function Home() {
     return (
         <>
             <LogoSection />
+            <Welcome />
             <Cards />
             <Footer />
         </>
