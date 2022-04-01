377841457fa5ee8c6bc3cda164e10ebeaaf320e7
Sarah <sarahlchavez@gmail.com>
2022-04-01 15:57:13 -0700

    added about page and text for other pages

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -5,13 +5,23 @@ import '../App.css'
 function BioSection() {
     return (
         <>
-            <div className='bio-container'>
-                <h1>Dr. Matthew Soden</h1>
-                <h3>Matt's Photo</h3>
-                <h3>Matthew Soden MD is a board-certified psychiatrist who is subspeciality trained in child and adolescent psychiatry. Dr. Soden graduated from Columbia University and Case Western Reserve University School of Medicine.  He completed residency training in pediatrics at UCSF Children’s Hospital Oakland and then went on to pursue psychiatry residency training at the University of Washington. Dr. Soden completed his fellowship training in child and adolescent psychiatry at Maine Medical Center.
-Dr. Soden and his wife are the proud parents of three children.
-</h3>
-            </div>
+            <section class="hero" id="about">
+                <img class="hero-img" loading="lazy" id="avatar" src='images/img-5.jpg' alt="Matthew Soden, M.D." />
+                    <div class="bio animate__animated animate__shakeX">
+                        <h2 class="bio-title">Matthew Soden, M.D.</h2>
+                        <br></br>
+                        <p class="bio-text">
+                        Matthew Soden MD is a board-certified psychiatrist who is subspeciality trained in child and adolescent psychiatry.<br></br>
+                        <br></br> 
+                        Dr. Soden graduated from Columbia University and Case Western Reserve University School of Medicine.  
+                        He completed residency training in pediatrics at UCSF Children’s Hospital Oakland and then went on to pursue psychiatry 
+                        residency training at the University of Washington.<br></br> 
+                        <br></br>
+                        Dr. Soden completed his fellowship training in child and adolescent 
+                        psychiatry at Maine Medical Center. Dr. Soden and his wife are the proud parents of three children.
+                        </p>
+                    </div>
+            </section>
         </>
 
     );
