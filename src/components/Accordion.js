df5d3fa0b9ecd64d7c17deda4e03f8f25553f120
Sarah <sarahlchavez@gmail.com>
2022-04-03 04:48:32 -0700

    added styled components in accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,9 +5,25 @@ import styled from 'styled-components';
 import { IconContext } from 'react-icons';
 import { FiPlus, FiMinus } from 'react-icons/fi';
 
-const AccordionSection = styled.div ``
+const AccordionSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    postion: relative;
+    height: 100vh;
+    background: #fff;
+    `;
 
-const AccordionContainer = styled.div ``
+const AccordionContainer = styled.div`
+    position: absolute;
+    top: 30%;
+    box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+`;
+
+const Wrap = styled.div`
+    background: #272727;
+`
 
 const Accordion = () => {
     return (    
@@ -17,7 +33,10 @@ const Accordion = () => {
                     {AccordionData.map((item, index) => {
                         return (
                             <>
+                            <Wrap>
                             <h1>{item.question}</h1>
+                            <span></span>
+                            </Wrap>
                             <p>{item.answer}</p>
                             </>
                         )
--- src/components/FaqsSection.js
+++ src/components/FaqsSection.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './FaqsSection.css'
 import '../App.css'
+import Accordion from './Accordion.js'
 
 function FaqsSection() {
     return (
         <>
             <div className='faqs-container'>
                 <h1>FAQs</h1>
-                <h2>Not Sure about this section</h2>
-                <h2>Health Insurance Info???</h2>
+                <Accordion />
             </div>
         </>
 
