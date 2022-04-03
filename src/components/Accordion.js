34d9907ca675a9cc80c03edd12e118278437c96f
Sarah <sarahlchavez@gmail.com>
2022-04-03 04:32:49 -0700

    building accordion section for faqs

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import './Accordion.css';
+import  { AccordionData } from './AccordionData';
+import styled from 'styled-components';
+import { IconContext } from 'react-icons';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+
+const AccordionSection = styled.div ``
+
+const AccordionContainer = styled.div ``
 
 const Accordion = () => {
-    return <div>accordion</div>
+    return (    
+        <IconContext.Provider value={{ color: '#000FFB9', size: '25px'}}>
+            <AccordionSection>
+                <AccordionContainer>
+                    {}
+                </AccordionContainer>
+            </AccordionSection >
+        </IconContext.Provider>
+    );
 };
 
 export default Accordion;
+
