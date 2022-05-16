5cf24b7ed8b88c60ade2b65379d347db9140ff14
Sarah <sarahlchavez@gmail.com>
2022-05-16 04:32:40 -0700

    updated file path for all images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
                         <CardItem 
-                            src='./resource-psych/images/img-5.jpg'
+                            src='./images/img-5.jpg'
                             text='Meet Dr. Soden'
                             // label='MeetDrSoden'
                             path='/meet-dr-soden'
                         />
                         <CardItem 
-                            src='./resource-psych/images/img-2.jpg'
+                            src='./images/img-2.jpg'
                             text='Services'
                             label='Services'
                             path='/services'
@@ -24,13 +24,13 @@ function Cards() {
                         {/* </ul>
                         <ul className='cards_items'> */}
                         <CardItem 
-                            src='./resource-psych/images/img-3.jpg'
+                            src='./images/img-3.jpg'
                             text='Scheduling'
                             label='Scheduling'
                             path='/scheduling'
                         />
                         <CardItem 
-                            src='./resource-psych/images/img-4.jpg'
+                            src='./images/img-4.jpg'
                             text='FAQs'
                             label='FAQs'
                             path='/faqs'
