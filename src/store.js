4c15a9c9c68768afd088a4b8b528b092cd2bd587
Sarah <sarahlchavez@gmail.com>
2022-05-22 12:08:57 -0700

    Updates

--- src/store.js
+++
@@ -1,6 +0,0 @@
-// from redux library need to import create store
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import notesReducer from "./reducers/notesReducer";
-
-export default createStore(notesReducer, applyMiddleware(thunk))
