85cab0bafe6bb419d1f75062654767fa101ba4b9
Sarah <sarahlchavez@gmail.com>
2022-05-31 16:15:35 -0700

    added addresses

--- src/components/SchedulingSection.js
+++ src/components/SchedulingSection.js
@@ -13,7 +13,25 @@ function SchedulingSection() {
                     <Button buttonStyle='btn--outline'>New Patients</Button>
                     <div id='contact-container'>
                         <p><tel>Phone: <a href="tel:833-807-2167">833-807-2167</a></tel></p>       
-                        <p><fax>Fax: <a href="fax:253-215-4399">253-215-4399</a></fax></p>      
+                        <p><fax>Fax: <a href="fax:253-215-4399">253-215-4399</a></fax></p>   
+                    </div>
+                    <div> 
+                    
+                    <ul className='address-container'>
+                        <h1>Locations</h1>
+                        <li>1300 Clay St, Suite 600
+                            Oakland, CA 94612
+                            833-807-2167
+                        </li>
+                        <li>501 S Cherry St, Suite 1100
+                            Denver, CO 80246
+                            833-807-2167
+                        </li>
+                        <li>8 S 1st Ave, Suite GG
+                            Yakima, WA 98902
+                            206-717-7393
+                        </li>
+                    </ul>
                     </div>
                     <ContactForm />
             </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,10 +5,11 @@ function Welcome() {
     return (
         <div className='welcome-container'>
             <h2>Welcome</h2>
-            <p>Resource Family Psychiatry is the private telepsychiatry practice of 
-            board-certified psychiatrist Matthew Soden, MD.<br></br>
-            Dr. Soden provides quality psychiatric care via video conferencing to 
-            patients of all ages in California, Colorado and Washington.</p><br></br> 
+            <p>Resource Family Psychiatry is the private practice of board-certified 
+            psychiatrist Matthew Soden, MD.<br></br>
+            Dr. Soden provides quality psychiatric care to patients of all ages in California, Colorado 
+            and Washington via secure video conferencing. There is limited availability for in person 
+            appointments.</p><br></br> 
             <div className='welcome-links'>
                 <div className="welcome-link-wrapper">
                    <div className="welcome-link-items"></div> 
