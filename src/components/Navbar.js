9535841002c4dfb5dd3be1f5a9a62a17d883c925
Sarah <sarahlchavez@gmail.com>
2022-06-06 11:00:20 -0700

    removed buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Button } from './Button';
+// import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+    // const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if(window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
+    // const showButton = () => {
+    //     if(window.innerWidth <= 960) {
+    //         setButton(false);
+    //     } else {
+    //         setButton(true);
+    //     }
+    // };
 
-    useEffect(() => {
-        showButton();
-    }, []);
+    // useEffect(() => {
+    //     showButton();
+    // }, []);
 
-    window.addEventListener('resize', showButton);
+    // window.addEventListener('resize', showButton);
     return (
         <>
             <nav className='navbar'>
@@ -44,17 +44,17 @@ function Navbar() {
                             <Link to='/scheduling' className='nav-links' onClick={closeMobileMenu}>Contact Us</Link></li>
                         <li className='nav-item'> 
                             <Link to='/faqs' className='nav-links' onClick={closeMobileMenu}>FAQs</Link></li>
-                        <li><a href='https://app2.luminello.com/matthewsodenmd/form' target="_blank" rel="noopener noreferrer" className='nav-links-mobile' onClick={closeMobileMenu}>
+                        {/* <li><a href='https://app2.luminello.com/matthewsodenmd/form' target="_blank" rel="noopener noreferrer" className='nav-links-mobile' onClick={closeMobileMenu}>
                             New Patients
                             </a></li>
                         <li><a href='https://app2.luminello.com/auth/login' target="_blank" rel="noopener noreferrer" className='nav-links-mobile' onClick={closeMobileMenu}>
                             Current Patients
-                            </a></li>
+                            </a></li> */}
                         {/* <li><a href="https://app2.luminello.com/matthewsodenmd/form" target="_blank" rel="noopener noreferrer"  className='nav-links-mobile' onClick={closeMobileMenu}>New Patients</a></li>
                         <li><a href="https://app2.luminello.com/auth/login" target="_blank" rel='noopener noreferrer' className='nav-links-mobile' onClick={closeMobileMenu}>Current Patients</a></li> */}
                     </ul>
-                    {button && <Button buttonStyle='btn--outline'>New Patients</Button>}
-                    {button && <Button buttonStyle='btn--outline'>Current Patients</Button>}
+                    {/* {button && <Button buttonStyle='btn--outline'>New Patients</Button>}
+                    {button && <Button buttonStyle='btn--outline'>Current Patients</Button>} */}
                 </div>
             </nav>
         </>
