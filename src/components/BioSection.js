b9638cad989a06a98ebce580d5687a46f23557ad
Sarah <sarahlchavez@gmail.com>
2022-06-06 10:00:06 -0700

    fixed typo in bio section, updated server file, deleted photo

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -10,7 +10,7 @@ function BioSection() {
                     <div className="bio-matt">
                         <h1 className="bio-title">Matthew Soden, MD</h1>
                         <p className="bio-text">
-                        Matthew Soden, MD is a board-certified psychiatrist who is subspeciality trained in child and adolescent psychiatry.
+                        Matthew Soden, MD is a board-certified psychiatrist who is subspecialty trained in child and adolescent psychiatry.
                         Dr. Soden graduated from Columbia University and Case Western Reserve University School of Medicine.  
                         He completed residency training in pediatrics at UCSF Children’s Hospital Oakland and then went on to pursue psychiatry 
                         residency training at the University of Washington. Dr. Soden completed his fellowship training in child and adolescent 
--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
-const express = require("express");
+//const express = require("express");
+import express from 'express';
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+import 'dotenv/config';
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(587, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
+  port: 587,
   auth: {
-    user: "***************@gmail.com",
-    pass: "********",
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
   },
 });
 
@@ -31,7 +35,7 @@ router.post("/contact", (req, res) => {
   const message = req.body.message; 
   const mail = {
     from: name,
-    to: "***************@gmail.com",
+    to: "sarahlchavez@gmail.com",
     subject: "Contact Form Message",
     html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
   };
