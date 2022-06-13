fdd6e6f1adf563f7470c14678db80e61b3a4b1e9
Sarah <sarahlchavez@gmail.com>
2022-06-13 06:05:18 -0700

    formatted photo in bio section to eliminate content jumping

--- src/components/AccordionData.js
+++ src/components/AccordionData.js
@@ -1,26 +1,22 @@
 
 export const AccordionData = [
     {
-        index: 0,
         question:'How do I book an appointment?', 
         answer: `Initially we will schedule a brief phone call prior to confirming your first appointment to discuss your goals. If I feel I can meet your needs, we might schedule an initial consultation together.\n 
 There are some severe mental health conditions that I am unable to care for in my private practice and might require the expertise or resources of another clinician or facility.\n
 If you would like to schedule a consultation, please contact us.`
     },
     {
-        index: 1,
         question:'Do you accept insurance?',
         answer: `I do not participate directly with insurance plans.`
     },
     {
-        index: 2,
         question:'What are your Payment Policies?', 
         answer: `You would make payments directly to Resource Family Psychiatry. We accept credit cards and ACH bank payments. Payment is due at the time of service.\n 
 Depending on your insurance coverage, all or a portion of your visit might be reimbursed by your insurance company. You will be provided with a “superbill” which is a type of receipt that you can give to your insurance company for reimbursement purposes. Please contact your insurance company directly to determine your specific coverage and complete any necessary paperwork.\n
 No shows/rescheduling with less than 24 business hours notice will be charged for the full session.`
     },
     {
-        index: 3,
         question:'What are your fees?',
         answer: `Initial consultation (2 sessions, each lasting 60 minutes):\n 
 First 60 minute appointment - evaluation including discussion with patient or guardian (if applicable), reviewing medical records and other collateral information: $295\n 
@@ -29,20 +25,11 @@ Follow-up appointment - 20 to 30 minutes including documentation time: $205\n
 Psychotherapy appointment - 45 to 50 minutes with or without medication management: $295\n`,       
         },
     {
-        index: 4,
         question:'Who do you see?', 
         answer: `Dr. Soden sees patients of all ages residing in the states of California, Colorado and Washington.\n
         `
     }
     ]
-
-    // {item.answer.split("Follow-up appointments").map((answer, index) => 
-    //                                 <>
-    //                                 {(index !== 0 && <strong>Follow-up Appointments</strong>)}
-    //                                 {answer}
-    //                                 </>
-
-    
     
 
     
