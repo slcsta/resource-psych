1559c0b420ce8728870544d874c6dbd134d81be5
Sarah <sarahlchavez@gmail.com>
2022-06-15 05:56:18 -0700

    addede psych today verified logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,7 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div className='footer-container'>
-            <div className='footer-links'>
-                <div className="footer-link-wrapper">
-                   <div className="footer-link-items">
-                   {/* <iframe class="_3Xz9Z" title="Embedded Content" name="htmlComp-iframe" width="100%" 
-                   height="100%" data-src="" 
-                   src="https://">  
-                   </iframe> */}
-
-                   
-                    </div> 
-                </div>
-            </div>
-        </div>
+        <div className='footer-container'></div> 
     )
 }
 
