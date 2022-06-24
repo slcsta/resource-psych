c4edbfc4fdca4902ea9a3e429f474650a0103051
Sarah <sarahlchavez@gmail.com>
2022-06-24 05:29:00 -0700

    added google analytics tag for ads

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-// import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    // const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    // const showButton = () => {
-    //     if(window.innerWidth <= 960) {
-    //         setButton(false);
-    //     } else {
-    //         setButton(true);
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     showButton();
-    // }, []);
-
-    // window.addEventListener('resize', showButton);
     return (
         <>
             <nav className='navbar'>
@@ -45,17 +30,7 @@ function Navbar() {
                             <Link to='/scheduling' className='nav-links' onClick={closeMobileMenu}>Contact Us</Link></li>
                         <li className='nav-item'> 
                             <Link to='/faqs' className='nav-links' onClick={closeMobileMenu}>FAQs</Link></li>
-                        {/* <li><a href='https://app2.luminello.com/matthewsodenmd/form' target="_blank" rel="noopener noreferrer" className='nav-links-mobile' onClick={closeMobileMenu}>
-                            New Patients
-                            </a></li>
-                        <li><a href='https://app2.luminello.com/auth/login' target="_blank" rel="noopener noreferrer" className='nav-links-mobile' onClick={closeMobileMenu}>
-                            Current Patients
-                            </a></li> */}
-                        {/* <li><a href="https://app2.luminello.com/matthewsodenmd/form" target="_blank" rel="noopener noreferrer"  className='nav-links-mobile' onClick={closeMobileMenu}>New Patients</a></li>
-                        <li><a href="https://app2.luminello.com/auth/login" target="_blank" rel='noopener noreferrer' className='nav-links-mobile' onClick={closeMobileMenu}>Current Patients</a></li> */}
                     </ul>
-                    {/* {button && <Button buttonStyle='btn--outline'>New Patients</Button>}
-                    {button && <Button buttonStyle='btn--outline'>Current Patients</Button>} */}
                 </div>
             </nav>
         </>
--- src/components/SchedulingSection.js
+++ src/components/SchedulingSection.js
@@ -2,15 +2,12 @@ import React from 'react';
 import './SchedulingSection.css'
 import '../App.css'
 import ContactForm from './ContactForm.js'
-// import { Button } from './Button.js';
-
 
 function SchedulingSection() {
     return (
         <>
             <div className='scheduling-container'>
                 <h1>Request a Consultation</h1>
-                    {/* <Button buttonStyle='btn--outline'>New Patients</Button> */}
                     <div id='contact-container'>
                         <p><tel>Phone: <a href="tel:833-807-2167">833-807-2167</a></tel></p>       
                         <p><fax>Fax: <a href="fax:253-215-4399">253-215-4399</a></fax></p>   
